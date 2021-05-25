7498f4d13bcb6b73196ce2166dae6f32a0f2bd93
Aung Ko Man <aungkoman@gmail.com>
2021-05-25 20:01:59 +0630

    header pdf back

--- generate.php
+++ generate.php
@@ -1,8 +1,8 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-//header("Content-type: application/pdf");
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+//error_reporting(E_ALL);
+header("Content-type: application/pdf");
 //header('Content-Type: text/html; charset=utf-8');
 require_once('./lib/tcpdf_min/tcpdf.php');
 require_once('./lib/fpdi/autoload.php');
